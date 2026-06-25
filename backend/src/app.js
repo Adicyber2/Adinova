b8adbd58e5707d4a97b78d130c5da5eef98cb6e1
Adicyber2 <adityadeshmukh1404@gmail.com>
2026-06-25 22:35:45 +0530

    frontend intigration done

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,8 @@ import cors from 'cors'
 import morgan from 'morgan'
 const app=express()
 
+app.use(express.static("./public"))
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
